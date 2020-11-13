70f8643dbc9754c00a28ada383e073dd0036e401
LeiHan-CA <53022586+LeiHan-CA@users.noreply.github.com>
2020-11-12 23:48:25 -0500

    add view files (ejs) home, about, projects, services, and contact

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lei Han Website Building Company' });
+});
+
+/* GET home page. */
+router.get('/home', (req, res, next)=> {
+  res.render('home', { title: 'home page'});
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next)=> {
+  res.render('about', { title: 'about'});
+});
+
+/* GET projects page. */
+router.get('/projects', (req, res, next)=> {
+  res.render('projects', { title: 'projects'});
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next)=> {
+  res.render('services', { title: 'services'});
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next)=> {
+  res.render('contact', { title: 'contact'});
 });
 
 module.exports = router;
